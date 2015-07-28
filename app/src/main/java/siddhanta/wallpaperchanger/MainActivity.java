aeddd5e92c6caf6d5e020840d389e02a2461569a
unknown <Sidd.c@Sidd-ADSC.ILLINOIS-SINGAPORE.LOCAL>
2015-07-28 16:44:40 +0800

    Commit from Android Studio

--- app/src/main/java/siddhanta/wallpaperchanger/MainActivity.java
+++ app/src/main/java/siddhanta/wallpaperchanger/MainActivity.java
@@ -45,6 +45,9 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
         final ActionBar actionBar = getActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
+
+
+
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
